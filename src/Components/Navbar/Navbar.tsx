5df0fa6f17e9701641d655b6c2566c03753e01d0
Chase Swick <chaseswick@gmail.com>
2024-11-08 01:57:47 -0500

    More routing

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,18 +3,20 @@ import logo from "./logo.png";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-interface Props {}
+interface Props { }
 
 const Navbar = (props: Props) => {
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-20">
-          <img src={logo} alt="" />
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
           <div className="hidden font-bold lg:flex">
-            <a href="" className="text-black hover:text-darkBlue">
-              Dashboard
-            </a>
+            <Link to="/search" className="text-black hover:text-darkBlue">
+              Search
+            </Link>
           </div>
         </div>
         <div className="hidden lg:flex items-center space-x-6 text-back">
