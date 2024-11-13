dd7070cf3ecec025bc1d4ea86feabc3122c759ae
Chase Swick <chaseswick@gmail.com>
2024-11-13 08:21:45 -0500

    Company Profile page

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -4,6 +4,8 @@ import HomePage from "../Pages/HomePage/HomePage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import SearchPage from "../Pages/SearchPage/SearchPage";
+import DesignPage from "../Pages/DesignPage/DesignPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +13,8 @@ export const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "", element: <HomePage /> },
+      { path: "search", element: <SearchPage /> },
+      { path: "design-guide", element: <DesignPage /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
