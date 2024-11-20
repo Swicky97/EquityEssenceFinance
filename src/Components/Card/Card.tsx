9850d3dbcc6bbc8f124ce924dbf3add1a9d7af30
Chase Swick <chaseswick@gmail.com>
2024-11-19 22:08:38 -0500

    Update links

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,7 +1,8 @@
 import React, { SyntheticEvent } from "react";
+import { Link } from "react-router-dom";
+import "./Card.css";
 import { CompanySearch } from "../../company";
 import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
-import { Link } from "react-router-dom";
 
 interface Props {
   id: string;
@@ -13,7 +14,7 @@ const Card: React.FC<Props> = ({
   id,
   searchResult,
   onPortfolioCreate,
-}: Props) => {
+}: Props): JSX.Element => {
   return (
     <div
       className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
@@ -21,13 +22,13 @@ const Card: React.FC<Props> = ({
       id={id}
     >
       <Link
-        to={`/company/${searchResult.symbol}`}
-        className="font-bold text-center text-black md:text-left"
+        to={`/company/${searchResult.symbol}/company-profile`}
+        className="font-bold text-center text-veryDarkViolet md:text-left"
       >
         {searchResult.name} ({searchResult.symbol})
       </Link>
-      <p className="text-black">{searchResult.currency}</p>
-      <p className="font-bold text-black">
+      <p className="text-veryDarkBlue">{searchResult.currency}</p>
+      <p className="font-bold text-veryDarkBlue">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
       <AddPortfolio
