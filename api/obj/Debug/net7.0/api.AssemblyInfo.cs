47f542110c6c7dbd071c5510437eac7a31ed1e5f
Chase Swick <chaseswick@gmail.com>
2024-12-23 22:21:22 -0500

    Run migrations

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f51d5918d5bbb466cce9007b2ad7974bad0d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3228ff7d5651c7f5aa7e656758e1e373b7e9e508")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
