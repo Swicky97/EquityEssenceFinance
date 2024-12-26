e9c6f5e2cff34ca478c3266243cdcbff680d9a15
Chase Swick <chase.swick@mandsconsulting.com>
2024-12-26 08:24:45 -0500

    New env setup

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3228ff7d5651c7f5aa7e656758e1e373b7e9e508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f542110c6c7dbd071c5510437eac7a31ed1e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
