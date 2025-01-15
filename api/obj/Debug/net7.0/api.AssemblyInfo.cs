37d1cb347e3b7c647dac06f651bc27ab83c99f08
Chase Swick <chase.swick@mandsconsulting.com>
2025-01-15 16:29:52 -0500

    Create portfolio controller

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f938ea500ee9fe083268334f7ca52e379a8f6e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9b23a52365c1676a94871fc6c92bbfb33a7b9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
