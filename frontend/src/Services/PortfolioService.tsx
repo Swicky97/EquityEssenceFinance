f677441ac6580103f1fee6fb1c28ec2e512fd0f4
Chase Swick <chase.swick@mandsconsulting.com>
2025-01-21 13:45:11 -0500

    Persist portfolio

---
+++ frontend/src/Services/PortfolioService.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { PortfolioGet, PortfolioPost } from "../Models/Portfolio";
+import { HandleError } from "../Helpers/ErrorHandler";
+
+const api = "http://localhost:5167/api/portfolio/";
+
+export const portfolioAddAPI = async (symbol: string) => {
+    try 
+    {
+        const data = await axios.post<PortfolioPost>(api + `?symbol=${symbol}`);
+        return data;
+    }
+    catch (error) 
+    {
+        HandleError(error);
+    }
+}
+
+export const portfolioGetAPI = async () => {
+    try 
+    {
+        const data = await axios.get<PortfolioGet[]>(api);
+        return data;
+    }
+    catch (error) 
+    {
+        HandleError(error);
+    }
+}
+
+export const portfolioDeleteAPI = async (symbol: string) => {
+    try 
+    {
+        const data = await axios.delete<PortfolioPost>(api + `?symbol=${symbol}`);
+        return data;
+    }
+    catch (error) 
+    {
+        HandleError(error);
+    }
+}
