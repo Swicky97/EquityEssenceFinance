b9dff5f59c6694e24794ed12fea75decb1a1235b
Chase Swick <chase.swick@mandsconsulting.com>
2025-01-23 11:55:38 -0500

    Update password reqs

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76ad7c6cdd032e6060805202cbd3acdbc4bdf37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28425e1d81d39dade1a5caef30b595a682034727")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -13,9 +13,15 @@ type RegisterFormsInputs = {
 };
 
 const validation = Yup.object().shape({
-  email: Yup.string().required("Email is required"),
+  email: Yup.string().required("Email is required").email("Invalid email"),
   userName: Yup.string().required("Username is required"),
-  password: Yup.string().required("Password is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters long")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[0-9]/, "Password must contain at least one number")
+    .matches(/[^a-zA-Z0-9]/, "Password must contain at least one special character"),
 });
 
 const RegisterPage = (props: Props) => {
