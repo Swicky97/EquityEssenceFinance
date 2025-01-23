28425e1d81d39dade1a5caef30b595a682034727
Chase Swick <chase.swick@mandsconsulting.com>
2025-01-23 11:46:37 -0500

    Bug fix signing key

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f677441ac6580103f1fee6fb1c28ec2e512fd0f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76ad7c6cdd032e6060805202cbd3acdbc4bdf37")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
