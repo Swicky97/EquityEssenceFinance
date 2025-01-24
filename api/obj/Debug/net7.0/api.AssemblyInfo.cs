cd2ac14c9b4b6d4a272aff430f6097c68ef2a395
Chase Swick <chaseswick@gmail.com>
2025-01-23 21:31:42 -0500

    Remove application.json files from tracking

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28425e1d81d39dade1a5caef30b595a682034727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e517a67bfbaaa9a6b13240fafc630a135c06d456")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
