7312e9b1bd9b2cbb3e146499e61b7354b458602d
Chase Swick <chase.swick@mandsconsulting.com>
2025-02-25 12:58:43 -0500

    Update assembly

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be72632727db365d7a4cd2b0b3dd4ce042cd5b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd99418625fd426f8dab635590894f526695f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
