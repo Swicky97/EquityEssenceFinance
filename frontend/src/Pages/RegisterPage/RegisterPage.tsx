2ae456fdee8f8d4884f12e3bb52b713cc8450703
Chase Swick <chase.swick@mandsconsulting.com>
2025-02-28 10:21:32 -0500

    Fix href links

--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -3,6 +3,7 @@ import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useAuth } from "../../Context/useAuth";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -115,12 +116,12 @@ const RegisterPage = (props: Props) => {
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
-                <a
-                  href="login"
+                <Link 
+                  to="/login"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Login
-                </a>
+                </Link>
               </p>
             </form>
           </div>
