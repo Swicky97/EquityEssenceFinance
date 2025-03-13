81bfdcd80d3fe61239c75e2f86e18b48cf60ccbd
Chase Swick <chase.swick@mandsconsulting.com>
2025-03-13 16:30:09 -0400

    Make appsettings.json optional

--- api/Program.cs
+++ api/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 // Add services to the container.
